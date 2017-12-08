d36cff4bf1181b279079043c2706de5d4eb35793
Deepak Adhikari <deeps.adhi@gmail.com>
2017-12-08 08:52:23 +0545

    Remove response of playlist controller

--- api/src/controllers/playlist.js
+++ api/src/controllers/playlist.js
@@ -1,13 +1,8 @@
 var playlistService = require('../services/playlist');
 
-module.exports = function(req, res) {
+module.exports = async function(req, res) {
   if (req.body.item.message.message && req.body.item.message.from &&
       req.body.item.message.date) {
-    res.json(playlistService.save(req.body));
+    await playlistService.save(req.body);
   }
-  // console.log(req.body);
-  // console.log('0-----')
-  // console.log(req.body.item.message.from);
-  // console.log('0-----')
-  // console.log(req.body.item.room.links);
 };
